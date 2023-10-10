2fc34f0ee8f04a1de4e12e8f44c1623e7bfc9da7
oyunseong <gkrlsanjf@naver.com>
2023-10-10 12:11:30 +0900

    폰트 추가

--- app/src/main/java/com/ostudio/relaxingsound/ui/theme/Type.kt
+++ app/src/main/java/com/ostudio/relaxingsound/ui/theme/Type.kt
@@ -1,10 +1,16 @@
 package com.ostudio.relaxingsound.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ostudio.relaxingsound.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -32,3 +38,25 @@ val Typography = Typography(
     )
     */
 )
+
+@Composable
+fun dpToSp(dp: Dp) = with(LocalDensity.current) { dp.toSp() }
+
+@Composable
+fun pxToDp(pixels: Float) = with(LocalDensity.current) { pixels.toDp() }
+
+val pretendardFontFamily = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_extrabold, FontWeight.ExtraBold),
+)
+
+val snackbarFontStyle: TextStyle
+    @Composable
+    get() = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontSize = dpToSp(16.dp),
+        lineHeight = dpToSp(24.dp),
+        letterSpacing = dpToSp((-0.2).dp)
+    )
