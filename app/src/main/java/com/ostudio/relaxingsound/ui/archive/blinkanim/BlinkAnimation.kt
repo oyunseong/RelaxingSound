fe7a7c18df1d0d7ad0c71b7495b6f831ab4b189b
oyunseong <gkrlsanjf@naver.com>
2023-10-17 11:23:04 +0900

    dir 생성 및 이동

--- app/src/main/java/com/ostudio/relaxingsound/ui/archive/blinkanim/BlinkAnimation.kt
+++ app/src/main/java/com/ostudio/relaxingsound/ui/archive/blinkanim/BlinkAnimation.kt
@@ -1,4 +1,4 @@
-package com.ostudio.relaxingsound.ui.archive
+package com.ostudio.relaxingsound.ui.archive.blinkanim
 
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
